10880aa84eeffb55ec8e50fbd17abe1197f95c17
Oleh Vanin <exsesx@gmail.com>
2022-03-26 20:29:06 +0200

    chore: better file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,11 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
