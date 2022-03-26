d1be032da28a6482c55c4a9908bbaa616766b1b5
Oleh Vanin <exsesx@gmail.com>
2022-03-27 00:57:57 +0200

    chore: add database auto-migration

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,12 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{migrate, PgPool};
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    migrate!();
+
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
