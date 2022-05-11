c4a2e2c064cc50e18ec139d78c2ddd8d10a5da5d
Oleh Vanin <exsesx@gmail.com>
2022-05-12 01:23:53 +0300

    chore: fmt

--- src/configuration.rs
+++ src/configuration.rs
@@ -9,16 +9,14 @@ use url::ParseError;
 
 static APP_ENVIRONMENT: &str = "APP_ENVIRONMENT";
 
-#[derive(Clone)]
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
     pub email_client: EmailClientSettings,
 }
 
-#[derive(Clone)]
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
 pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
@@ -40,16 +38,14 @@ impl EmailClientSettings {
     }
 }
 
-#[derive(Clone)]
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
 pub struct ApplicationSettings {
     pub host: String,
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
 }
 
-#[derive(Clone)]
-#[derive(serde::Deserialize)]
+#[derive(Clone, serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
