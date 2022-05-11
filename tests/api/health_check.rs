b576e593b8a51355924aec59e5b6a6b7a1f924a0
Oleh Vanin <exsesx@gmail.com>
2022-05-11 23:17:18 +0300

    feat: create more comfy folder structure for tests

---
+++ tests/api/health_check.rs
@@ -0,0 +1,19 @@
+use crate::helpers::spawn_app;
+
+#[actix_web::test]
+async fn health_check_works() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute health check request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
